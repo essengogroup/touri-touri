3a81c977dbca392bdf33db525087923ae006dfb0
styve Lioumba <styvelioumba98@gmail.com>
2021-09-26 15:50:42 +0200

    splash screen fonctionnel

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:touritouri/screens/index.dart';
 
@@ -11,12 +12,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: 'touri-touri',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Index(),
+      home: AnimatedSplashScreen(
+          duration: 2500,
+          splash: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon( Icons.home,
+                size: 50,
+              ),
+              Text("Touri-Touri",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white,
+                )
+              ),
+            ],
+          ),
+          nextScreen: const Index(),
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor: Colors.blue
+      )
     );
   }
 }
