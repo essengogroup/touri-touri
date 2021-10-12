6dcebbac19782bef0e813ec5a722385288eb2e16
styve Lioumba <styvelioumba98@gmail.com>
2021-10-12 13:36:23 +0200

    typo region screen

--- lib/screens/home_page/region.dart
+++ lib/screens/home_page/region.dart
@@ -11,73 +11,69 @@ class Region extends StatefulWidget {
 class _RegionState extends State<Region> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(
-          child: Stack(
-            children: [
-
-              Container(
-                margin: EdgeInsets.only(bottom: 100.0,top: 30.0),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: List.generate(
-                        3,
-                        (index) => Container(
-                              margin: const EdgeInsets.only(
-                                  left: 20, right: 15, top: 20, bottom: 20),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      offset: Offset(0, 4),
-                                      blurRadius: 20,
-                                      color: Color(0xFFB0CCE1).withOpacity(0.32),
-                                    )
-                                  ]),
-                              child: Material(
-                                color: Colors.transparent,
-                                child: InkWell(
-                                  onTap: null,
-                                  child: Padding(
-                                    padding: EdgeInsets.all(20.0),
-                                    child: Column(
-                                      children: [
-                                        Container(
-                                          margin: EdgeInsets.only(bottom: 15),
-                                          padding: const EdgeInsets.all(25),
-                                          decoration: BoxDecoration(
-                                              // color: Colors.blue.withOpacity(0.13),
-                                              // shape: BoxShape.circle
-                                          ),
-                                          child: ClipRRect(
-                                              borderRadius: BorderRadius.circular(100),
-                                              child: Image.asset('assets/datas/r$index.jpg',width: 200,height: 200,fit: BoxFit.cover,)),
-                                        ),
-                                        Text('region $index'),
-                                        SizedBox(height: 10),
-                                        Text(
-                                          "3 sites à visiter",
-                                          style: TextStyle(fontSize: 12),
-                                        )
-                                      ],
-                                    ),
-                                  ),
+    return Center(
+      heightFactor: MediaQuery.of(context).size.height,
+      widthFactor: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+        child: Container(
+          height: 400,
+          width: MediaQuery.of(context).size.width,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(
+                  3,
+                      (index) => Container(
+                    margin: const EdgeInsets.only(
+                        left: 20, right: 15, top: 20, bottom: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 4),
+                            blurRadius: 20,
+                            color: Color(0xFFB0CCE1).withOpacity(0.32),
+                          )
+                        ]),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: null,
+                        child: Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(bottom: 15),
+                                padding: const EdgeInsets.all(25),
+                                decoration: BoxDecoration(
+                                  // color: Colors.blue.withOpacity(0.13),
+                                  // shape: BoxShape.circle
                                 ),
+                                child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(100),
+                                    child: Image.asset('assets/datas/r$index.jpg',width: 200,height: 200,fit: BoxFit.cover,)),
                               ),
-                            )),
-                  ),
-                ),
-              ),
-            ],
+                              Text('region $index'),
+                              SizedBox(height: 10),
+                              Text(
+                                "3 sites à visiter",
+                                style: TextStyle(fontSize: 12),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )),
+            ),
           ),
         ),
-      ],
+      ),
     );
+
   }
 
 
