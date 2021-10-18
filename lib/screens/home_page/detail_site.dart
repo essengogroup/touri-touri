7ec7d4bb7d61023481c72b7f2825b45adb6c51ad
benji_pc <benaja.bendo02@gmailcom>
2021-10-18 16:27:44 +0200

    modification de la card et vue detail du site

--- lib/screens/home_page/detail_site.dart
+++ lib/screens/home_page/detail_site.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class DetailSite extends StatefulWidget {
-  const DetailSite({Key? key}) : super(key: key);
+  final int? index;
+
+  const DetailSite({Key? key, required this.index}) : super(key: key);
 
   @override
   _DetailSiteState createState() => _DetailSiteState();
@@ -10,6 +13,187 @@ class DetailSite extends StatefulWidget {
 class _DetailSiteState extends State<DetailSite> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(200),
+        child: AppBar(
+          title: Text('nom du site'),
+          centerTitle: true,
+          flexibleSpace: Stack(
+            children: [
+              SizedBox(
+                height: 400,
+                width: MediaQuery.of(context).size.width,
+                child: ClipRRect(
+                  child: Image.asset(
+                    'assets/datas/r0.jpg',
+                    fit: BoxFit.cover,
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                ),
+              ),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.blue.withOpacity(0.4),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Row(
+              children: [
+                Row(
+                  children: [
+                    RatingBarIndicator(
+                      rating: 2.5,
+                      itemBuilder: (context, index) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      itemCount: 5,
+                      itemSize: 16.0,
+                      direction: Axis.horizontal,
+                    ),
+                    SizedBox(
+                      width: 4.0,
+                    ),
+                    Text('2.5'),
+                  ],
+                ),
+                Spacer(),
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.favorite_outlined,
+                  ),
+                  label: Text('32'),
+                ),
+              ],
+            ),
+          ),
+          Wrap(
+            crossAxisAlignment: WrapCrossAlignment.center,
+            //direction: Axis.vertical,
+            alignment: WrapAlignment.spaceBetween,
+            verticalDirection: VerticalDirection.down,
+
+            //textDirection: TextDirection.rtl,
+            spacing: 5.0,
+            children: const <Widget>[
+              Chip(
+                label: Text("c#"),
+                padding: EdgeInsets.all(10.0),
+              ),
+              Chip(
+                label: Text("java"),
+              ),
+              Chip(
+                label: Text("css"),
+              ),
+              Chip(
+                label: Text("html"),
+              ),
+              Chip(
+                label: Text("kodsfsltiln"),
+              ),
+              Chip(
+                label: Text("css"),
+              ),
+              Chip(
+                label: Text("html"),
+              ),
+              Chip(
+                label: Text("kodsfsltiln"),
+              ),
+              Chip(
+                label: Text("css"),
+              ),
+              Chip(
+                label: Text("html"),
+              ),
+              Chip(
+                label: Text("kodsfsltiln"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+   /* return Scaffold(
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                    title: Text("No; du site",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        )),
+                    background: Image.network(
+                      "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                      fit: BoxFit.cover,
+                    )),
+              ),
+              SliverPersistentHeader(
+                delegate: _SliverAppBarDelegate(
+                  TabBar(
+                    labelColor: Colors.black87,
+                    unselectedLabelColor: Colors.grey,
+                    tabs: [
+                      Tab(icon: Icon(Icons.info), text: "Tab 1"),
+                      Tab(icon: Icon(Icons.lightbulb_outline), text: "Tab 2"),
+                    ],
+                  ),
+                ),
+                pinned: true,
+              ),
+            ];
+          },
+          body: Center(
+            child: Text("Sample text"),
+          ),
+        ),
+      ),
+    );*/
   }
 }
+/*
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
+  }
+}*/
--- lib/widgets/build_card_site.dart
+++ lib/widgets/build_card_site.dart
@@ -1,54 +1,139 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:touritouri/screens/home_page/detail_site.dart';
 
 Widget builCardSite(int index, BuildContext context) {
-  return Container(
-    margin: const EdgeInsets.only(bottom: 8),
-    height: 185,
-    width: double.infinity,
-    child: Stack(
+  return Card(
+    margin: EdgeInsets.only(bottom: 8.0),
+    child: Column(
       children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Image.asset(
-            'assets/datas/r$index.jpg',
-            fit: BoxFit.cover,
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
+        SizedBox(
+          height: 185,
+          width: double.infinity,
+          child: Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(
+                  'assets/datas/r$index.jpg',
+                  fit: BoxFit.cover,
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                ),
+              ),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        PageTransition(
+                            type: PageTransitionType.scale,
+                            alignment: Alignment.bottomCenter,
+                            duration: const Duration(seconds: 1),
+                            child:  DetailSite(index: index)));
+                  },
+                  splashColor: Colors.blue.withOpacity(0.4),
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.black26,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Site touristique $index',
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          'Description du site $index',
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        Material(
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: (){},
-            splashColor: Colors.blue.withOpacity(0.4),
-            child: Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.black26,
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Nom du site Touristique',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                softWrap: false,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              Row(
                 children: [
-                  Text( 'Site touristique $index',
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold
-                    ),),
-                  const SizedBox(height: 6),
-                  Text( 'Description du site $index',
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400
-                    ),),
+                  RatingBarIndicator(
+                    rating: 2.5,
+                    itemBuilder: (context, index) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    itemCount: 5,
+                    itemSize: 16.0,
+                    direction: Axis.horizontal,
+                  ),
+                  SizedBox(
+                    width: 4.0,
+                  ),
+                  Text('2.5'),
                 ],
               ),
-            ),
+              Text(
+                'Description et detail du site',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                softWrap: false,
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.w400),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextButton.icon(
+                      onPressed: (){},
+                      icon: Icon(Icons.favorite_outlined,),
+                      label: Text('32'),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextButton.icon(
+                      onPressed: (){},
+                      icon: Icon(
+                        Icons.insert_comment_outlined,
+                      ),
+                      label: Text('32'),
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
-        ),
+        )
       ],
     ),
   );
